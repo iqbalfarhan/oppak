b2ade50a26efefe484288003c94147eccbcf1985
iqbal syuhada <iqbalfarhan1996@gmail.com>
2024-05-16 10:39:02 +0800

    update loading image

--- resources/views/livewire/pages/profile.blade.php
+++ resources/views/livewire/pages/profile.blade.php
@@ -5,12 +5,14 @@
 
     <div class="card max-w-2xl mx-auto card-divider">
         <form class="card-body" wire:submit="simpan">
-            <div class="flex w-full justify-center">
+            <div class="flex flex-col w-full items-center">
                 <div class="avatar" onclick="document.getElementById('openCamera').click()">
                     <div class="w-32 rounded-full btn-bordered">
                         <img src="{{ $photo ? $photo->temporaryUrl() : $user->image }}" />
                     </div>
                 </div>
+                <div class="text-xs" wire:loading wire:target="photo">Uploading...</div>
+                <div class="text-xs" wire:loading wire:target="camera">Uploading...</div>
             </div>
             <input type="file" wire:model="camera" id="openCamera" class="hidden" accept="image/*" capture="user">
             <div class="py-6 grid md:grid-cols-2 gap-x-6 gap-y-2">
