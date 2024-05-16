99be4bcf57c9eb9d1a0c2832c7d02ade0ad1fff9
iqbal syuhada <iqbalfarhan1996@gmail.com>
2024-05-16 12:26:29 +0800

    offline

--- resources/views/vendor/laravelpwa/offline.blade.php
+++ resources/views/vendor/laravelpwa/offline.blade.php
@@ -1,10 +1,8 @@
 @extends('print.blank')
 
 @section('content')
-    <div class="page-wrapper">
-        <div class="flex flex-col h-screen w-full items-center justify-center bg-base-300 gap-6">
-            <div class="font-bold text-5xl mb-4 text-base-content">No Connection!</div>
-            <p>Anda saat ini tidak terhubung ke jaringan mana pun.</p>
-        </div>
+    <div class="flex flex-col h-screen w-full items-center justify-center bg-base-300 gap-6 p-6 text-center">
+        <div class="font-bold text-5xl mb-4 text-base-content">No connection!</div>
+        <p>Anda saat ini tidak terhubung ke jaringan mana pun.</p>
     </div>
 @endsection
