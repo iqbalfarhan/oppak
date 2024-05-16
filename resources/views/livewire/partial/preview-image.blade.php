f61cc0a4f7704aad1d5f3ef14b3e44ba33eeecb4
iqbal syuhada <iqbalfarhan1996@gmail.com>
2024-05-16 12:18:50 +0800

    update previewimage

--- resources/views/livewire/partial/preview-image.blade.php
+++ resources/views/livewire/partial/preview-image.blade.php
@@ -2,7 +2,9 @@
     <input type="checkbox" class="modal-toggle" @checked($url ? true : false) />
     <div class="modal" role="dialog">
         <div class="modal-box max-w-3xl p-0">
-            <img class="w-full" src="{{ Storage::url($url) }}" alt="{{ $url }}">
+            @if ($url)
+                <img class="w-full" src="{{ Storage::url($url) }}" alt="{{ $url }}">
+            @endif
         </div>
         <button class="modal-backdrop" wire:click="closeModal">Close</button>
     </div>
