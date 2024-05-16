5f3a6f50851893c2b71b3216e78635b84d3d1377
iqbal syuhada <iqbalfarhan1996@gmail.com>
2024-05-16 12:24:34 +0800

    update offline

--- resources/views/vendor/laravelpwa/offline.blade.php
+++ resources/views/vendor/laravelpwa/offline.blade.php
@@ -1,7 +1,7 @@
 @extends('print.blank')
 
 @section('content')
-    <div class="flex h-screen w-full items-center justify-center bg-base-300 gap-6">
+    <div class="flex flex-col h-screen w-full items-center justify-center bg-base-300 gap-6">
         <div class="font-bold text-5xl mb-4 text-base-content">No Connection!</div>
         <p>Anda saat ini tidak terhubung ke jaringan mana pun.</p>
     </div>
